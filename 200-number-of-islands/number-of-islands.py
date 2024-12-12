29165de0c3b354ea501df023638ed10d4cfc4dd1
Naman <50843800+gentleman101@users.noreply.github.com>
2024-12-12 08:47:07 +0530

    Time: 253 ms (43.50%) | Memory: 19.9 MB (42.21%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -3,28 +3,27 @@ class Solution:
         rows = len(grid)
         columns = len(grid[0])
         islands = 0
-        visited = [[False for _ in range(columns)] for _ in range(rows)]
-        movements = [(1,0),(0,1),(-1,0),(0,-1)]
+        visited = [[False for i in range(columns)] for j in range(rows)]
+                    
+        movements = [(1,0),(0,1),(0,-1),(-1,0)]
 
-        def dfs (row,col):
-            visited[row][col]=True
+
+
+        def dfs(i,j):
+            visited[i][j]=True
             for move in movements:
-                nrow = row + move[0]
-                ncol = col + move[1]
-                if (
-                    0 <= nrow < rows
-                    and 0 <= ncol < columns
-                    and not visited[nrow][ncol]
-                    and grid[nrow][ncol] == "1"
-                ):
-                    dfs(nrow,ncol)
+                nrow = i+move[0]
+                ncol = j+move[1]
+
+                if 0 <= nrow < rows and 0 <= ncol < columns and grid[nrow][ncol] == "1" and not visited[nrow][ncol]:
+                    dfs(nrow, ncol)
 
         for row in range(rows):
             for col in range(columns):
-                if grid[row][col] == "1" and not visited[row][col]:
-                    islands +=1
+                if (grid[row][col]=="1") & (not visited[row][col]):
+                    islands+=1
                     dfs(row,col)
+
 
         return islands
 
-        
