1a4dfa142f89a5f6a88f698ede45d1e967be1470
Naman <50843800+gentleman101@users.noreply.github.com>
2024-12-17 09:07:03 +0530

    Time: 503 ms (45.10%) | Memory: 43.1 MB (28.77%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,6 +1,6 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        # dp = [[0 for _ in range(len(text2)+1)] for _ in range(len(text1)+1)]
+        dp = [[-1 for _ in range(len(text2)+1)] for _ in range(len(text1)+1)]
         # def recurString(indText1,indText2):
         #     # Base Case
 
@@ -26,33 +26,24 @@ class Solution:
 
 
         # Tabulation
-    # Create a DP table of size (n+1) x (m+1) initialized with -1
-        s1 = text1
-        s2 = text2
-        n = len(s1)
-        m = len(s2)
-        dp = [[-1 for j in range(m + 1)] for i in range(n + 1)]
+        n = len(text1)
+        m = len(text2)
 
-        # Initialize the base cases:
-        # - The length of LCS with an empty string is 0, so dp[i][0] = 0 for all i
-        # - The length of LCS with an empty string is 0, so dp[0][j] = 0 for all j
-        for i in range(n + 1):
+        for i in range(n+1):
             dp[i][0] = 0
-        for j in range(m + 1):
+
+        for j in range(m+1):
             dp[0][j] = 0
 
-        # Fill in the DP table by considering characters from both strings
-        for ind1 in range(1, n + 1):
-            for ind2 in range(1, m + 1):
-                if s1[ind1 - 1] == s2[ind2 - 1]:
-                    # If the characters match, increment the LCS length
-                    dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1]
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+
+                if text1[i-1]==text2[j-1]:
+                    dp[i][j] = 1+ dp[i-1][j-1]
+
                 else:
-                    # If the characters do not match, take the maximum of
-                    # LCS length without one character from s1 or s2
-                    dp[ind1][ind2] = max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1])
-        
-        # The value in dp[n][m] represents the length of the Longest Common Subsequence
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1])
+
         return dp[n][m]
 
 
