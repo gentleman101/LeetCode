49f78a120f3efdd58df37d3834587a023206c063
Naman <50843800+gentleman101@users.noreply.github.com>
2024-12-18 12:45:45 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (6.06%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,17 +1,29 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        # n = len(nums)
+        # dp = [-1 for _ in range(n)]
+        # def utilRob(i):
+        #     if i==0:
+        #         return nums[i]
+        #     if i<0:
+        #         return 0
+        #     if dp[i]!=-1:
+        #         return dp[i]
+        #     notTake = utilRob(i-1)
+        #     Take = nums[i] + utilRob(i-2)
+        #     dp[i] = max(Take,notTake)
+        #     return dp[i]
+        # return utilRob(n-1)
+
         n = len(nums)
-        dp = [-1 for _ in range(n)]
-        def utilRob(i):
-            if i==0:
-                return nums[i]
-            if i<0:
-                return 0
-            if dp[i]!=-1:
-                return dp[i]
-            notTake = utilRob(i-1)
-            Take = nums[i] + utilRob(i-2)
+        dp = [0 for _ in range(n)]
+        dp[0]=nums[0]
+        
+        for i in range(1,n):
+            notTake = dp[i-1]
+            if i>0:
+                Take = nums[i] + dp[i-2]
             dp[i] = max(Take,notTake)
-            return dp[i]
-        return utilRob(n-1)
+        return dp[n-1]
+
         
