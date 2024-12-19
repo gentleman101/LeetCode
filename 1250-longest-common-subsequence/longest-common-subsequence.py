8a8acfcd791a23e3597b6d0bcd3900419947aa3c
Naman <50843800+gentleman101@users.noreply.github.com>
2024-12-19 09:52:50 +0530

    Time: 734 ms (28.19%) | Memory: 43.6 MB (23.15%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -2,22 +2,25 @@ class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         n = len(text1)
         m = len(text2)
-        dp = [[-1 for _ in range(m)] for _ in range(n)]
-        def utilLCS(i,j):
-            if i<0 or j<0:
-                return 0
-            if dp[i][j]!=-1:
-                return dp[i][j]
-            matching =0
-            if text1[i]==text2[j]:
-                matching = 1 + utilLCS(i-1,j-1)
-            notMatching = -1e9
-            if text1[i]!=text2[j]:
-                notMatching = max(utilLCS(i-1,j),utilLCS(i,j-1))
-            dp[i][j] = max(matching,notMatching)
-            return dp[i][j]
+        dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
 
-        return utilLCS(n-1,m-1)
+        for i in range(n+1):
+            dp[i][0] = 0
+
+        for j in range(m+1):
+            dp[0][j] = 0
+
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                matching = 0
+                notMatching = -1e9
+                if text1[i-1]==text2[j-1]:
+                    matching = 1 + dp[i-1][j-1]
+                else:
+                    notMatching = max(dp[i-1][j],dp[i][j-1])
+                dp[i][j] = max(matching,notMatching)
+
+        return dp[n][m]
 
 
 
