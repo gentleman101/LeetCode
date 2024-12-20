75ae328f35700b475a4bea4b90221eac18838d3b
Naman <50843800+gentleman101@users.noreply.github.com>
2024-12-20 13:41:54 +0530

    Time: 140 ms (50.75%) | Memory: 23.5 MB (38.01%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -2,19 +2,20 @@ class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
 
         n = len(prices)
-        dp = [[0 for _ in range(2)] for _ in range(len(prices)+1)]
 
-
-
-
+        # Two states for current and future
+        ahead = [0, 0]  # ahead[0]: can't buy, ahead[1]: can buy
+        curr = [0, 0]
+        
         for i in range(n-1,-1,-1):
             for buyFlag in [0,1]:
                 if buyFlag:
-                    profit = max(dp[i+1][True],-prices[i]+dp[i+1][False])
+                    profit = max(ahead[True],-prices[i]+ahead[False])
 
                 else:
 
-                    profit = max(dp[i+1][False],prices[i]-fee+dp[i+1][True])
-                dp[i][buyFlag] = profit
-        return dp[0][1]
+                    profit = max(ahead[False],prices[i]-fee+ahead[True])
+                curr[buyFlag] = profit
+            ahead = curr
+        return ahead[1]
         
