1a7f06d0c1429d2fd83f6adec78874ac56ab5015
Naman <50843800+gentleman101@users.noreply.github.com>
2024-12-25 19:28:33 +0530

    Time: 17 ms (96.89%) | Memory: 32 MB (7.83%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+
+        maxi = -sys.maxsize-1 # maximum sum
+        sum = 0
+
+        for i in range(len(nums)):
+            sum += nums[i]
+
+            if sum > maxi:
+                maxi = sum
+
+            # If sum < 0: discard the sum calculated
+            if sum < 0:
+                sum = 0
+        return maxi
+
+
+        
