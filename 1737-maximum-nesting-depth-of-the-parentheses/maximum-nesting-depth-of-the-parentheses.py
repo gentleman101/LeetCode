57d4c06566538c81d9c93bb46b289d458f559495
Naman <50843800+gentleman101@users.noreply.github.com>
2024-12-30 08:45:08 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (5.93%) - LeetSync

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxDepth(self, s: str) -> int:
+
+        openCounter = 0
+        maxCounter = 0
+        for char in s:
+            if char==')':
+                openCounter-=1
+            elif char=='(':
+                openCounter+=1
+                maxCounter = max(maxCounter,openCounter)
+                
+
+        return maxCounter
