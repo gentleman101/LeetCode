ed328679930a8438fd42a87f47376a931d6f70f2
Naman <50843800+gentleman101@users.noreply.github.com>
2024-12-31 19:29:58 +0530

    Time: 293 ms (6.21%) | Memory: 31.3 MB (46.24%) - LeetSync

---
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -0,0 +1,31 @@
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        if not heights:
+            return 0
+
+        # PSE (Previous Smaller Element)
+        pse = [-1] * len(heights)
+        pse_stack = []
+        for i in range(len(heights)):
+            while pse_stack and heights[i] <= pse_stack[-1][0]:
+                pse_stack.pop()
+            pse[i] = pse_stack[-1][1] if pse_stack else -1
+            pse_stack.append((heights[i], i))
+
+        # NSE (Next Smaller Element)
+        nse = [len(heights)] * len(heights)
+        nse_stack = []
+        for i in range(len(heights) - 1, -1, -1):
+            while nse_stack and heights[i] <= nse_stack[-1][0]:
+                nse_stack.pop()
+            nse[i] = nse_stack[-1][1] if nse_stack else len(heights)
+            nse_stack.append((heights[i], i))
+
+        # Calculate max area
+        max_area = 0
+        for i in range(len(heights)):
+            width = nse[i] - pse[i] - 1
+            area = width * heights[i]
+            max_area = max(max_area, area)
+
+        return max_area
