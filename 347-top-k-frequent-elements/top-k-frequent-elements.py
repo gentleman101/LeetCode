b465b890428f3ebb64d8977e1018c0f4f24f9c9d
Naman <50843800+gentleman101@users.noreply.github.com>
2025-01-03 08:44:42 +0530

    Time: 3 ms (91.92%) | Memory: 21.3 MB (29.45%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,16 +1,15 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # countDict = defaultdict(list)
-        countList = [[] for _ in range(len(nums)+1)]
-        for i in set(nums):
-            cnt  = nums.count(i)
-            countList[cnt].append(i)
+        counter = Counter(nums)
+
+        maxHeap = [[-1*freq,num] for num,freq in counter.items()]
+        heapify(maxHeap)
+        i=0
         res = []
-        for i in range(len(countList)-1,0,-1):
+        while i<k:
+            res.append(heappop(maxHeap)[1])
+            i+=1
 
-            res.extend(countList[i])
-            if k==len(res):
-                return res
+        return res
 
 
-        
