269de7c6ac64389c9146bc36bf5a1c8d5acaa1eb
Naman <50843800+gentleman101@users.noreply.github.com>
2025-01-05 13:12:59 +0530

    Time: 3 ms (88.97%) | Memory: 19.2 MB (21.56%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,31 +1,34 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         adj = defaultdict(list)
-        indegrees = [0]*numCourses
-        q = deque()
-        res = []
+        visited = [False] * numCourses
+        path  = [False] * numCourses
+
+
         for c1,c2 in prerequisites:
             adj[c2].append(c1)
 
-        for courses in range(numCourses):
-            for i in adj[courses]:
-                indegrees[i]+=1
-
-        for degrees in range(numCourses):
-            if indegrees[degrees]==0:
-                q.append(degrees)
-
-        while q:
-            node = q.popleft()
-            res.append(node)
-
-            for neighbours in adj[node]:
-                indegrees[neighbours]-=1
-                if indegrees[neighbours]==0:
-                    q.append(neighbours)
 
 
-        return len(res)==numCourses
+        def dfs(c):
+            visited[c] = True
+            path[c] = True
 
+            for neighbour in adj[c]:
+                if not visited[neighbour]:
+                    if dfs(neighbour)==False:
+                        return False
+                else:
+                    if path[neighbour]:
+                        return False
 
+            path[c] = False
+            return True
 
+        for course in range(numCourses):
+            if not visited[course]:
+                if not  dfs(course):
+                    return False
+        
+
+        return True
